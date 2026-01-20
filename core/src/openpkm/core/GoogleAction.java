f29e4b0f9a7feecc0daf636d6460163c238f306c
Rok Koren <rokoren@gmail.com>
2026-01-21 00:06:42 +0100

    Application redesign

---
+++ core/src/openpkm/core/GoogleAction.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/NetBeansModuleDevelopment-files/actionListener.java to edit this template
+ */
+package openpkm.core;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Logger;
+import org.openide.awt.ActionID;
+import org.openide.awt.ActionReference;
+import org.openide.awt.ActionReferences;
+import org.openide.awt.ActionRegistration;
+import org.openide.util.NbBundle.Messages;
+import org.openide.windows.TopComponent;
+
+@ActionID(
+        category = "OpenPKM",
+        id = "openpkm.core.GoogleAction"
+)
+@ActionRegistration(
+        iconBase = "openpkm/core/resources/google.png",
+        displayName = "#CTL_GoogleAction"
+)
+@ActionReferences({
+    @ActionReference(path = "Toolbars/OpenPKM", position = 0),
+    @ActionReference(path = "Shortcuts", name = "D-0")
+})
+@Messages("CTL_GoogleAction=Google")
+public final class GoogleAction implements ActionListener
+{
+    private static final Logger LOG = Logger.getLogger(GoogleAction.class.getName());     
+    
+    @Override
+    public void actionPerformed(ActionEvent e) 
+    {
+        // TODO implement action body      
+        TopComponent tc = new GoogleTopComponent();
+        tc.open();
+        tc.requestActive();
+    }     
+}
